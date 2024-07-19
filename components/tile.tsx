b45905a7c88bfc9fdb3e2f798537dd52ed1347cc
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-07-19 16:52:42 +0200

    format

--- components/tile.tsx
+++ components/tile.tsx
@@ -1,14 +1,14 @@
-import {Tile as TileProps} from "@/models/tile"
+import { Tile as TileProps } from "@/models/tile";
 import { containerWidht, tileCountPerDimension } from "@/constants";
 import styles from "@/styles/tile.module.css";
 
 function Tile({ position, value }: TileProps) {
-    // Függvény a pozíció pixelértékre való átváltásához
+  // Függvény a pozíció pixelértékre való átváltásához
   const positionPixels = (position: number) => {
     // Pozíció arányos átváltása pixelértékre
     return (position / tileCountPerDimension) * containerWidht;
   };
-// CSS stílus objektum létrehozása a bal és felső pozíció meghatározásához
+  // CSS stílus objektum létrehozása a bal és felső pozíció meghatározásához
   const style = {
     left: positionPixels(position[0]), // Bal pozíció pixelben
     top: positionPixels(position[1]), // Felső pozíció pixelben
