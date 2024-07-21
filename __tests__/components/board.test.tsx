a22a156addac027ced4166460169e919fb17e7ba
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-07-21 18:45:43 +0200

    add context

--- __tests__/components/board.test.tsx
+++ __tests__/components/board.test.tsx
@@ -1,11 +1,16 @@
 import Board from "@/components/board";
+import GameProvider from "@/context/game-context";
 import { render } from "@testing-library/react";
 
 describe("Board", () => {
   // Meghatároz egy tesztesetet, amely azt vizsgálja, hogy a Board 16 cellával renderelődik-e
   it("should render Board with 16 cells", () => {
     // Rendereli a Board komponenst, és kinyeri a DOM elemet tartalmazó container objektumot
-    const { container } = render(<Board />);
+    const { container } = render(
+      <GameProvider>
+        <Board />
+      </GameProvider>,
+    );
     // Kikeresi az összes olyan elemet a containerben, amelynek az osztályneve "cell"
     const cellElements = container.querySelectorAll(".cell");
 
@@ -15,7 +20,12 @@ describe("Board", () => {
 
   it("should render Board with 2 tiles", () => {
     // Leírja a teszt nevét: "A tábla 2 csempével kell renderelődjön"
-    const { container } = render(<Board />); // Rendereli a Board komponenst és kinyeri a container elemet a renderelési eredményből
+    const { container } = render(
+      <GameProvider>
+        <Board />
+      </GameProvider>,
+    );
+    // Rendereli a Board komponenst és kinyeri a container elemet a renderelési eredményből
     const tiles = container.querySelectorAll(".tile"); // Kiválasztja az összes elemet a container-ből, amelyeknek az osztálya "tile"
 
     expect(tiles.length).toEqual(2); // Elvárás: az összes "tile" osztályú elem számának 2-nek kell lennie
