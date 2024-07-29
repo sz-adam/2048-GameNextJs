a4dfc95ab2f431dfce8fc154644fe2ae86fd9f7a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-07-29 20:23:15 +0200

    add media responsive

--- constants.ts
+++ constants.ts
@@ -1,6 +1,8 @@
 export const tileCountPerDimension = 4;
 
-export const containerWidht = 288; //px
+export const containerWidhtMobile = 288; //px
+
+export const containerWidhtDesktop = 464; //px
 
 export const mergeAnimationDuration = 100; //ms
 
